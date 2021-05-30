253c0a0e35fd0a348a4d828eb571d31dcb497d22
M1ke <mygitrepo0@gmail.com>
2021-05-30 16:23:18 +0200

    Initial Commit

---
+++ CiceSports/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CiceSports
+//
+//  Created by M1keDev on 30/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
